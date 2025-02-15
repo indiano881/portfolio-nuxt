a61d790fb36bd80f1e5da018897d89aefc8c5a1b
Davide Baldi <d.baldi88@gmail.com>
2025-02-15 19:08:46 +0100

    click event at nose finally works

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,34 @@
     </h1>
     <h1>INTERNAZIONALIZATION + CONTINUE ON THE POKER THEME !</h1>
     <div class="max-w-sm mx-auto">
-      <div class="relative aspect-square ">
+      <div class="relative aspect-square " :style="{ transform: `rotate(${-eyeOffsetX * 0.5}deg)` }">
         <img src="/casino/dealer.svg"  alt="Davide" class="absolute inset-0 w-full h-full object-contain z-10 " style="top: 0%;left: 0%;width: 70%;"/>
-        <!-- Right eye -->
-        <img src="/eye.svg" alt="eye"class="absolute z-20"style="top: 0%;left: -15%;width: 100%;" :style="{transform: `translate(${eyeOffsetX}px, ${eyeOffsetY}px)`  }"/>
+        <!-- Glasses -->
+        <img src="/glasses2.svg" alt="eye"class="absolute z-20"style="top: 26%;left: 20%;width: 30%;" />
         <!-- Left eye -->
-        <img src="/eye.svg" alt="eye" class="absolute z-20" style="top: 0%; left: -25%; width: 100%; " :style="{transform: `translate(${eyeOffsetX}px, ${eyeOffsetY}px)`  }"/>
-        <!-- nose -->
-        <img src="/nose3.svg" alt="nose3" class="absolute z-20 w-[20px]" style="top: 43%; left: 32%; width: 6%;" @click="()=>console.log('click')"/>
+        <img src="/eye.svg" alt="eye"class="absolute z-20"style="top: 8%;left: -15%;width: 80%;" :style="{transform: `translate(${eyeOffsetX}px, ${eyeOffsetY}px)`  }"/>
+        <!-- Right eye -->
+        <img src="/eye.svg" alt="eye" class="absolute z-20" style="top: 8%; left: -4%; width: 80%; " :style="{transform: `translate(${eyeOffsetX}px, ${eyeOffsetY}px)`  }"/>
+        <!-- Nose (wrapped in a button) -->
+    <button
+      @click="handleClick"
+      class="absolute z-30"
+      style="
+        top: 40%; 
+        left: 30%; 
+        width: 10%;
+        background: transparent; 
+        border: none;
+        cursor: pointer;
+      "
+    >
+      <img
+        src="/nose4.svg"
+        alt="nose"
+        class="w-full h-auto"
+      />
+    </button>
+        
         <!-- mouth -->
         <img src="/mouth.svg" alt="mouth" class="absolute z-20 w-[20px]" style="top: 41%; left: 20%; width: 30%;" />
       </div>
@@ -37,11 +57,13 @@ const handleMouseMove = (e) => {
   const offsetY = e.clientY - centerY
 
   // 3) Scale down the movement so the eye only moves slightly
-  const ratio = 0.02
+  const ratio = 0.015
   eyeOffsetX.value = offsetX * ratio
   eyeOffsetY.value = offsetY * ratio
 }
-
+function handleClick() {
+  console.log('click')
+}
 // Attach the event listener when component mounts
 onMounted(() => {
   window.addEventListener('mousemove', handleMouseMove)
